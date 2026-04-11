0fa0e87be4cc13ec9f0b076e25728d06bf59c580
Bibbeep <habibalfrb@gmail.com>
2026-04-11 16:46:54 +0700

    added rust lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -243,7 +243,7 @@ return {
 			prismals = {},
 			gopls = {},
 			-- pyright = {},
-			-- rust_analyzer = {},
+			rust_analyzer = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
 			-- Some languages (like typescript) have entire language plugins that can be useful:
